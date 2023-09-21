ef63a99a0c8208b1e50ee978e2cfa89cf444d23b
Codesinc <codes.inc.com@gmail.com>
2023-09-21 10:47:38 +0500

    code updated one

--- pages/become-a-teacher.js
+++ pages/become-a-teacher.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageBanner from '../components/Common/PageBanner';
-// import PremiumAccessTwo from '../components/Common/PremiumAccessTwo';
+import PremiumAccessTwo from '../components/Common/PremiumAccessTwo';
 import FunFactsThree from '../components/Common/FunFactsThree';
 import ApplyAsInstructor from '../components/BecomeATeacher/ApplyAsInstructor';
 import RegisterForm from '../components/BecomeATeacher/RegisterForm';
